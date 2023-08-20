3f836b0ba59cc71a6eebe6a9d2869468c1a04c39
Hamzamt99 <tamarihamza4@gmail.com>
2023-08-20 17:45:46 +0000

    changing the class components into function components and some styling in scss

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,6 @@
+import './style.scss'
+function Footer() {
+  return <footer>&copy; 2018</footer>;
+}
+
+export default Footer;
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,10 @@
+import './style.scss'
+function Header() {
+  return (
+    <header>
+      <h1>RESTy</h1>
+    </header>
+  );
+}
+
+export default Header;
