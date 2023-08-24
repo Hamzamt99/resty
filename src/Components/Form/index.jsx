4d91467cc834f2edd20b979580930fd6663f9e80
Hamzamt99 <tamarihamza4@gmail.com>
2023-08-24 14:14:11 +0000

    done adding the style for the rest method

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,43 +1,66 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './Form.scss';
 
 function Form(props) {
+  const [value, setvalue] = useState('get');
 
-  const [value, setvalue] = useState('get')
-
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
       method: value,
       url: e.target.url.value,
-      data: e.target.text.value
+      data: e.target.text.value,
     };
-    
+
     props.handleApiCall(formData);
-  }
-  const checkMethod = e => {
-    return setvalue(e.target.id)
-  }
+  };
+
+  const checkMethod = (e) => {
+    setvalue(e.target.id);
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <label >
+        <label>
           <span>URL: </span>
-          <input name='url' type='text' />
+          <input name="url" type="text" />
           <button type="submit">GO!</button>
         </label>
         <label className="methods">
-          <span id="get" onClick={checkMethod}>GET</span>
-          <span id="post" onClick={checkMethod}>POST</span>
-          <span id="put" onClick={checkMethod}>PUT</span>
-          <span id="delete" onClick={checkMethod}>DELETE</span>
+          <span
+            id="get"
+            onClick={checkMethod}
+            className={value === 'get' ? 'active' : ''}
+          >
+            GET
+          </span>
+          <span
+            id="post"
+            onClick={checkMethod}
+            className={value === 'post' ? 'active' : ''}
+          >
+            POST
+          </span>
+          <span
+            id="put"
+            onClick={checkMethod}
+            className={value === 'put' ? 'active' : ''}
+          >
+            PUT
+          </span>
+          <span
+            id="delete"
+            onClick={checkMethod}
+            className={value === 'delete' ? 'active' : ''}
+          >
+            DELETE
+          </span>
         </label>
-        <textarea name='text'></textarea>
+        <textarea name="text"></textarea>
       </form>
     </>
   );
-
 }
-
 
 export default Form;
--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,12 +1,18 @@
-import './style.scss'
+import './style.scss';
+
 function Results(props) {
   return (
     <section>
-      {
-        props.loading ?
-          (props.data ? <pre>{JSON.stringify(props.data, undefined, 2)}</pre> : 'loading...') :
-          null
-      }
+      {props.loading ? (
+        props.data ? (
+          <div>
+            <pre className='header'>{JSON.stringify({ Headers: props.header }, undefined, 2)}</pre>
+            <pre  className='data'>{JSON.stringify(props.data, undefined, 2)}</pre>
+          </div>
+        ) : (
+          'loading...'
+        )
+      ) : null}
     </section>
   );
 }
