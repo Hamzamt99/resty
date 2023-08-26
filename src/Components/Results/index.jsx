b4fb36b23aa7e3cd8ec5902163398e511f09a3d0
Hamzamt99 <tamarihamza4@gmail.com>
2023-08-26 13:05:26 +0000

    done the styling and hooks

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,18 +1,28 @@
+import React from 'react';
 import './style.scss';
 
 function Results(props) {
+  function next() {
+    props.handleApiCall({ url: props.response.data.next });
+  }
+  function previous() {
+    props.handleApiCall({ url: props.response.data.previous });
+  }
   return (
-    <section>
+    <section className="results">
       {props.loading ? (
-        props.data ? (
-          <div>
-            <pre className='header'>{JSON.stringify({ Headers: props.header }, undefined, 2)}</pre>
-            <pre  className='data'>{JSON.stringify(props.data, undefined, 2)}</pre>
+        <div>
+          <pre className="header">{JSON.stringify({ Headers: props.header }, undefined, 2)}</pre>
+          <pre className="data">{JSON.stringify(props.response.data, undefined, 2)}</pre>
+
+          <div className="button-container">
+            {props.response.data.previous && <button onClick={previous}>Previous</button>}
+            {props.response.data.next && <button onClick={next}>Next</button>}
           </div>
-        ) : (
-          'loading...'
-        )
-      ) : null}
+        </div>
+      ) : (
+        <div className="loading">loading...</div>
+      )}
     </section>
   );
 }
